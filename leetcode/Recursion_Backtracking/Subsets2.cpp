2bec51efb66cf272549d8e2fd4ed858fbfeab0ba
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-22 21:26:11 +0530

    Update Subsets2.cpp

--- leetcode/Recursion_Backtracking/Subsets2.cpp
+++ leetcode/Recursion_Backtracking/Subsets2.cpp
@@ -1,3 +1,17 @@
+/*
+If no duplicates, this style of code generates
+[[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3],[]]
+If we have [1,2,2]
+The 3 gets replaced by 2.
+Hence we need to remove
+[[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3],[]]
+                 |         |        |
+So, in each step, we either choose or don't choose and go to next element.
+If the next element is same, we skip it.
+
+Also, there can be more than one duplicates. So, use a while loop.
+
+*/
 class Solution {
 public:
     int flag  = 0;
@@ -13,15 +27,12 @@ public:
         p.pop_back(); // restore
 
     
-        if(p.size() > 0 && p.back() == nums[i])return ; //  Here, we cut off the branch where the last number was same.
-        
+        while(i + 1 < nums.size() && nums[i] == nums[i+1])i++;
+        // [1,2,3] is now [1,2,2]. So,
         // not choosing or moving forward
         dfs(subsets, nums, i + 1 , p);
     }
-    
-    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
         vector<vector<int>> subsets;
         vector<int> p;
         sort(nums.begin(), nums.end());
