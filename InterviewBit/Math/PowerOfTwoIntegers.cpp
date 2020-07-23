935cfb5a40a827f45861befd387c6b26e68ce8b6
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-23 13:05:20 +0530

    Update PowerOfTwoIntegers.cpp

--- InterviewBit/Math/PowerOfTwoIntegers.cpp
+++ InterviewBit/Math/PowerOfTwoIntegers.cpp
@@ -1,6 +1,9 @@
 /*
 This was an easy maths question.
 
+Approach 1: Factor based sqrt(n) approach.
+Approach 2: Log(n) approach
+
 To find if A ^ P is possible,
 we search all factors of A. All the factors of a number
 can be found by iterating only till sqrt(A).
@@ -16,6 +19,7 @@ O(sqrt(n)) to find factors.
 log(n) required to calculate power. So, O(sqrt(n) * log(n))
 */
 
+/*Approach 1*/
 
 int Solution::isPower(int A) {
     
@@ -48,3 +52,37 @@ int Solution::isPower(int A) {
     
     return 0;
 }
+
+
+/*Approach 2*/
+
+/*
+a = x ^ y
+log a = y log x (by property of log)
+log a / log x = y
+So, if y is an integer, there exists such a combination where a = x ^ y
+*/
+
+bool isInt(float x)
+{
+    if(ceil(x) == floor(x))return true;
+    else return false;
+}
+
+int Solution::isPower(int A) {
+    
+    int num = A;
+    if(A == 1)return 1;
+    for(int i = 2; i <= sqrt(A); i++)
+    {
+        if( isInt(log(A)/log(i)) )
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+
+
