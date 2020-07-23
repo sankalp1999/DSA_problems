701319c65f79f1c93360dc86a4f4809fc06011e9
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-23 18:05:51 +0530

    Create RecursiveSolution.cpp

---
+++ InterviewBit/LinkedLists/ReverseNodesInSizeOfK/RecursiveSolution.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head, int k, int& swaps)
+    {
+        if(swaps==0)return head;
+        swaps--;
+        ListNode* current = head;
+        ListNode* next = NULL;
+        ListNode* prev = NULL;
+        int count = 0;
+        while(current && count < k)
+        {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+            count++;
+        }
+        
+        if(next!=NULL)
+            head->next = reverse(next, k, swaps);
+        return prev;
+    }
+    
+    
+    
+    
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        
+        int len = 0;
+        ListNode* curr = head;
+        while(curr)
+        {
+            curr = curr->next;
+            len++;
+        }
+        int swaps = len/k;
+        return reverse(head, k, swaps);
+        
+        
+    }
+};
