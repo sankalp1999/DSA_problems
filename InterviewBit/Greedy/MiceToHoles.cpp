81908cd90d543c6570a4c7419b88fd1d2d30d2d9
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-27 20:50:04 +0530

    Update MiceToHoles.cpp

--- InterviewBit/Greedy/MiceToHoles.cpp
+++ InterviewBit/Greedy/MiceToHoles.cpp
@@ -1,21 +1,23 @@
 /*
-We use greedy strategy here. We see that the safe move is that we allot mice to their nearest holes.
+Main Idea
+- The minimum time for each mouse is achieved by getting to the closest hole. So, we allot the closest holes to the mice.
+- Consequently, the maximum minimum difference is our answer.
+
+Safe Move : Allot mice to their nearest holes.
 This way, it will take minimum time for each of the mice to reach to their hole.
-The maximum time for some mice i to reach it's hole i is our result.
-Sort both the vectors.
-Then, find maximum difference.
+
 */
 
 
 int Solution::mice(vector<int> &A, vector<int> &B) {
     
-    sort(A.begin(), A.end());
-    sort(B.begin(), B.end());
+    sort(A.begin(), A.end()); // Sort both the vectors
+    sort(B.begin(), B.end()); 
     
     int diff = 0;
     for(int i = 0; i < A.size(); i++)
     {
-        diff = max(diff, abs(A[i] - B[i]));
+        diff = max(diff, abs(A[i] - B[i])); // Allot each mice to nearest hole.
     }
 
     return diff;
