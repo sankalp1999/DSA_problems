41d79a9f533b90ee573846e52d4c40be3d16a30e
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-27 14:08:40 +0530

    Update and rename MergeIntervals.cpp to MergeIntervals_Extra_Space_Nlogn_Solution.cpp

--- leetcode/Intervals/MergeIntervals_Extra_Space_Nlogn_Solution.cpp
+++ leetcode/Intervals/MergeIntervals_Extra_Space_Nlogn_Solution.cpp
@@ -1,7 +1,7 @@
 /*
 The main idea in this question is to check the start and end using if conditions.
 Handle duplicates to save computation.
-
+*/
 
 class Solution {
 public:
@@ -11,7 +11,7 @@ public:
         vector<int> visited(v.size(), 0);
         
         if(v.size() <= 1) return v;
-        sort(v.begin(), v.end()); // Using  a set here gives better runtime as it handles duplicates and sorts also.
+        sort(v.begin(), v.end());
         
         int start = v[0][0];
         int end =   v[0][1];
@@ -20,16 +20,17 @@ public:
         {
             if(i + 1 < v.size() && v[i][0] == v[i+1][0] && v[i][1] == v[i+1][1] )continue;
             { 
-                if(end < v[i][0]) // If our current end is less than start of next, then time change start and push the current result.
+                if(end < v[i][0])
                 {
                  res.push_back(vector<int>{start, end});
                  start = v[i][0];
+                 end = v[i][1];
                 }
-                if(start == v[i][0] && v[i][1] <= end ) // If start is same and ith end is still less than end, continue
+                if(start == v[i][0] && v[i][1] <= end ) // No need to check this  but to make things clearer
                 {
                     continue;
                 }
-                else if(start==v[i][0] && v[i][1] > end) // If start is same and end is higher than current end, update end.
+                else if(start==v[i][0] && v[i][1] > end) // No need to check this also
                 {
                     end = v[i][1];
                 }
