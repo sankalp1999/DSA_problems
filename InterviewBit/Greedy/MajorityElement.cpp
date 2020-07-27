fb1eccca64e93ebcd5491156d1387024e958106c
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-27 20:35:35 +0530

    Update MajorityElement.cpp

--- InterviewBit/Greedy/MajorityElement.cpp
+++ InterviewBit/Greedy/MajorityElement.cpp
@@ -6,14 +6,14 @@ https://leetcode.com/articles/majority-element/
 int Solution::majorityElement(const vector<int> &A) {
     
     int count = 0;
-    int candidate = 0;
+    int candidate = 0; // We choose a candidate
     for(int i = 0; i < A.size(); i++)
     {
-        if(count == 0)
+        if(count == 0) // Choose a new candidate
         {
             candidate = A[i];
         }
-        count += (A[i] == candidate ? 1 : -1);
+        count += (A[i] == candidate ? 1 : -1); // We vote for that candidate if the array element 
     }
     return candidate;
 }
