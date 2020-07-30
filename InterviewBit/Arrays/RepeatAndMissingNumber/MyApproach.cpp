fac5206d5617eccec5fefef61e0bc11e047a72ce
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-30 20:09:45 +0530

    Create MyApproach.cpp

---
+++ InterviewBit/Arrays/RepeatAndMissingNumber/MyApproach.cpp
@@ -0,0 +1,35 @@
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    
+    long long int K, C;
+    K = 0;
+    C = 0;
+    for(long long int i = 1; i <= A.size(); i++)
+    {
+        K = K + ( (i) - (long long)A[i-1] );
+        C = C + (  (i) * (i) -  (long long)A[i-1] * (long long)A[i-1] );
+    }
+
+    vector<int> res;
+    long long int B = (long long)(C - K * K)/(2 * K);
+    long long int a = K + B;
+    
+    int flag  =0;
+    int flag2 = 0;    
+    for(int i = 0; i < A.size(); i++)
+    {
+        if(A[i] == a)flag++;
+        if(A[i]== B)flag2++;
+    }
+    if(flag==2)
+    {
+        res.push_back(a);
+        res.push_back(B);
+    }
+    else
+    {
+        res.push_back(B);
+        res.push_back(a);
+    }
+    return res;
+    
+}
