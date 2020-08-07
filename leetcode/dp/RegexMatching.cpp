98c37d86773998c1c525e6cb5c09592e287446f9
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-08-08 01:05:58 +0530

    Update RegexMatching.cpp

--- leetcode/dp/RegexMatching.cpp
+++ leetcode/dp/RegexMatching.cpp
@@ -1,3 +1,12 @@
+/*
+ '*' can repeat previous element as many times or even remove it.
+  ab*
+  will match to abb
+  ab*
+  will match to a also.
+*/
+
+
 class Solution {
 
 public:
@@ -16,7 +25,7 @@ public:
             count++;
             if(p[i-1] == '*') 
             {
-                dp[0][i] = dp[0][i-2];
+                dp[0][i] = dp[0][i-2]; // Exclude previous character if that helps.
             }
 
         }
@@ -25,16 +34,16 @@ public:
         {
             for(int j = 1; j <= p.size(); j++)
             {
-                if(s[i-1] == p[j-1] || p[j-1] == '.')
+                if(s[i-1] == p[j-1] || p[j-1] == '.') // Check smaller substring/previous sequeence same as LCS
                 {
                     dp[i][j] = dp[i-1][j-1]; 
                 }
-                else if(p[j-1] == '*')
+                else if(p[j-1] == '*') // MAIN CASE
                 {
-                    dp[i][j] = dp[i][j-2];
-                    if(j-2>=0 && s[i-1] == p[j-2] || p[j-2] == '.')
-                    {
-                        dp[i][j] = dp[i][j] | dp[i-1][j];
+                    dp[i][j] = dp[i][j-2];// If we want to exclude last character, just take that state
+                    if(j-2>=0 && s[i-1] == p[j-2] || p[j-2] == '.') // Another case is when we want to repeat previous character
+                    {                                               // Only possible when preceding element is equal to ith string element
+                        dp[i][j] = dp[i][j] | dp[i-1][j];   // Take the above state's value since we will match that. (Not clear)
                     }
                 }
                 else
