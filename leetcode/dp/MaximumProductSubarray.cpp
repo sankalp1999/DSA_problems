b705994be3251f0c146fd09d712a32c92512fa7d
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-08-11 12:05:58 +0530

    Update MaximumProductSubarray.cpp

--- leetcode/dp/MaximumProductSubarray.cpp
+++ leetcode/dp/MaximumProductSubarray.cpp
@@ -1,24 +1,35 @@
-class Solution {
-public:
-    
-    /*
+/*
 Extension of kadane algorithm. In this question,
 we have three choices at each point,
 1. If the current element is positive, we will multiply it with curr_max_prod
 2. IF the current element is negative, it will make our curr_max_prod negative if curr_max prod is positive and negative number can act as curr_min, so we maintain another variable that is the curr_min product. Now, if we multiply negative element with curr_min_prod, it can give a larger number
 3. We want to start a new subarray
+
+
+very nice . I guess a bit of more explanation would help people understand:
+
+this is very similar to the " max cumulative sum subarray" problem. here you keep 2 values: the max cumulative product UP TO current element starting from SOMEWHERE in the past, and the minimum cumuliative product UP TO current element . it would be easier to see the DP structure if we store these 2 values for each index, like maxProduct[i],minProduct[i] .
+
+at each new element, u could either add the new element to the existing product, or start fresh the product from current index (wipe out previous results), hence the 2 Math.max() lines.
+
+if we see a negative number, the "candidate" for max should instead become the previous min product, because a bigger number multiplied by negative becomes smaller, hence the swap()
+
+
 */
+
+
+/*
+-1, 6, 2 , -2
+   ^ At 2, we have running product as -12. But if we multiply with -2,
+   we get our max prod. I should have thought about the negative case 
+   separately. This question was doable.
+   so, we keep another variable curr_min_prod and prev_min_prod
+*/
+
     
-    
-    /*
-    -1, 6, 2 , -2
-           ^ At 2, we have running product as -12. But if we multiply with -2,
-           we get our max prod. I should have thought about the negative case 
-           separately. This question was doable.
-           so, we keep another variable curr_min_prod and prev_min_prod
-    */
-    
-    
+
+class Solution {
+public:
     
     int maxProduct(vector<int>& nums) {
         int curr_max_prod, curr_min_prod;
