54ef37406d24232e251b368540e584b1d823b71d
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-08-17 00:17:18 +0530

    Update Sudoku.cpp

--- InterviewBit/Recursion_Backtracking/Sudoku.cpp
+++ InterviewBit/Recursion_Backtracking/Sudoku.cpp
@@ -1,9 +1,25 @@
+/*
+Initially, I had implemented two for loops where I would move forward to next state if 
+a value could be placed on the current state and I was using a count variable to 
+keep track of recursion. This was not a proper approach -
+1. It did not allow me to backtrack properly. 
+2. The base cases were not proper.
+
+The below code is proper. It travels on the board row by row.
+If current state is valid, we move on to next state. If some state
+is invalid, then we try to return a false value and backtrack to 
+the previous state and try other combinations.
+
+*/
+
+
+
+
 
 bool isValid(int i, int j, char check, vector<vector<char>>& A)
 {
     int v_offset = i/3; 
     int h_offset = j/3;
-    
     
     // grid check
     for(int l = v_offset * 3; l < v_offset * 3 + 3; l++)
@@ -62,7 +78,7 @@ bool solver(vector<vector<char>>& A, int row, int col)
       if( isValid(row,col,c,A) )
       {
          A[row][col] = c;
-         if(solver(A, row, col + 1))
+         if(solver(A, row, col + 1)) // Try next state. We have to reach end by going back and forth.
          {
              return true;
          }
@@ -72,12 +88,10 @@ bool solver(vector<vector<char>>& A, int row, int col)
    
    // If not able to place, backtrack.
    return false;
-   
 }
 
 void Solution::solveSudoku(vector<vector<char> > &A) {
 
     int count = 0;
-
     solver(A,0,0);
 }
