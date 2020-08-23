9b57e40cbe886e266692894c48bc85b6b02e4d69
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-08-23 12:13:20 +0530

    Update CheckBST.cpp

--- HackerRank_HackerEarth_Other_plaforms/Trees/CheckBST.cpp
+++ HackerRank_HackerEarth_Other_plaforms/Trees/CheckBST.cpp
@@ -2,25 +2,27 @@
 Check for limits as well as
 duplication
 */
-  bool helper(Node* root, int min_val, int max_val)
-  {
-      if(root == NULL)
-          return true;
+bool helper(Node* root, int min_val, int max_val)
+{
+	if(root == NULL)
+  		return true;
 
-      bool left = helper(root->left,min_val, root->data) ;
+	bool left = helper(root->left,min_val, root->data) ;
 
-      bool right = helper(root->right,root->data,max_val);
+	bool right = helper(root->right,root->data,max_val);
 
-      // equal to is for checking duplication also
-      if(root->data <= min_val || root->data >= max_val) 
-      {
-          return false;
-      }
+	// equal to is for checking duplication also
+	if(root->data <= min_val || root->data >= max_val) 
+	{
+	  return false;
+	}
 
-      return left && right;   
-  }
-bool checkBST(Node* root) {
-int min_val = -1e5;
-int max_val = 1e5;
-	return helper(root, min_val, max_val);
+	return left && right;   
+	}
+
+bool checkBST(Node* root) 
+{
+    int min_val = -1e5;
+    int max_val = 1e5;
+    return helper(root, min_val, max_val);
 }
