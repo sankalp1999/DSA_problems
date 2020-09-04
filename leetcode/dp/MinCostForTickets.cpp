af022a02e9969df234475e49e0f2f106967c74c5
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-09-04 22:37:45 +0530

    Update MinCostForTickets.cpp

--- leetcode/dp/MinCostForTickets.cpp
+++ leetcode/dp/MinCostForTickets.cpp
@@ -9,23 +9,22 @@ public:
         int n = days.size();
         vector<int> dp(n + 1, 1e5);
         
-        dp[0] = 0;
+        dp[0] = 0; // 0th day is going to be 0
         for(int i = 1; i <= days.size(); i++)
         {
             
             for(int j = 1; j < i; j++)
             {
-               
                 if(days[i-1] - days[j-1] < 7)
                 {
-                    dp[i] = min(dp[i], dp[j-1] + costs[1]);
+                    dp[i] = min(dp[i], dp[j-1] + costs[1]); // Buy a 7 day pass from 8th past day
                 }
                 if(days[i-1] - days[j-1] < 30)
                 {
-                    dp[i] = min(dp[i], dp[j-1] + costs[2]);
+                    dp[i] = min(dp[i], dp[j-1] + costs[2]); // Buy a 30 day pass from 31st day back
                 }
             }
-            dp[i] = min(dp[i], dp[i-1] + min({costs[0], costs[1], costs[2]}));
+            dp[i] = min(dp[i], dp[i-1] + min({costs[0], costs[1], costs[2]})); // Single day case
         }
         return dp[n];
      
