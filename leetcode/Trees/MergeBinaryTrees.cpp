0a4cd3b39f9308490fbbf35cc8fbef0e92803505
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-10-29 11:41:52 +0530

    Create MergeBinaryTrees.cpp

---
+++ leetcode/Trees/MergeBinaryTrees.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   /* 
+   Do a preorder traversal
+   Just modifying the root of the tree t1.
+   If both present then add.
+   Otherwise, return the existing node and make it t1's
+   */
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(t1 == NULL)return t2;
+        if(t2 == NULL)return t1;
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
+    }
+};
