3630e96f3ab7015ca22f17c64da7d78512d1a3b2
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2021-07-25 19:39:38 +0530

    Update FindMedianFromRunningData.cpp

--- leetcode/Heap_Map/FindMedianFromRunningData.cpp
+++ leetcode/Heap_Map/FindMedianFromRunningData.cpp
@@ -1,4 +1,3 @@
-
 /*
 Main idea
 1. Use a maxheap to maintain the first half of array. This way, you will always have the mid available at the top.
@@ -12,73 +11,63 @@ Balancing
 If there is a size difference of more than two, then pop out one element from the heap having 
 more elements and push that element into the smaller heap.
 
+NOTE: When size is unbalanced, we send the min of minheap or max of maxheap to the other part.
+This does not change the answer.
+
 Finding Median
 If the size of the list is even, then take half of sum of both the tops.
 Otherwise, just return the heap's top which is having more elements.
 
 */
 
-
-
-
 class MedianFinder {
 public:
     /** initialize your data structure here. */
-    priority_queue<int,vector<int>, greater<int>> minheap;
-    priority_queue<int> maxheap;
+     priority_queue<int> leftHeap; // Max Heap
+    priority_queue<int,vector<int>,greater<int>> rightHeap;
     MedianFinder() {
-        while(!minheap.empty())minheap.pop();
-        while(!maxheap.empty())maxheap.pop();
+       
     }
     
     void addNum(int num) {
-       
-
-        if(minheap.empty() && maxheap.empty())
+        if(leftHeap.empty())
         {
-            minheap.push(num);return;
-        }
-        if(!maxheap.empty() && num <= maxheap.top())
-        {
-            maxheap.push(num);
-        }
-        else 
-        {
-            minheap.push(num);
+            leftHeap.push(num);
+            return;
         }
         
-        if((int)maxheap.size() - (int)minheap.size() >= 2)
+        if(leftHeap.top() > num) // It belongs to the left half
         {
-            
-            minheap.push(maxheap.top());
-            maxheap.pop();
+            leftHeap.push(num);
         }
-        else if((int)minheap.size() - (int)maxheap.size() >= 2)
-        {
-           
-            maxheap.push(minheap.top());
-            minheap.pop();
-        }
+        else rightHeap.push(num); // right half
         
+        
+        // If size not equal, then sending the max or min element
+        // won't change the structure.
+        // 1 3    5 5 6
+        if(leftHeap.size() > rightHeap.size() + 1)
+        {
+            rightHeap.push(leftHeap.top());
+            leftHeap.pop();
+        }
+        else if(rightHeap.size() > leftHeap.size() + 1 )
+        {
+            leftHeap.push(rightHeap.top());
+            rightHeap.pop();
+        }
     }
     
     double findMedian() {
-        int n = (int)minheap.size() + (int)maxheap.size();
-        if( n % 2 == 0 && n != 0){
-            int med = minheap.top() + maxheap.top();
-            return (med)/2.0 ;
-        }
-        else{
-            double res  = minheap.size() >  maxheap.size() ? minheap.top() : maxheap.top();
-            return res;
-        }
         
+        if(rightHeap.empty() )return leftHeap.top();
+        if(leftHeap.size() != rightHeap.size() )
+        {
+            return leftHeap.size() > rightHeap.size() ? leftHeap.top() : rightHeap.top();
+        }
+        int a = leftHeap.top();
+        int b= rightHeap.top();
+        return ( (a*1.0 + b) / 2.0);
     }
 };
 
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
